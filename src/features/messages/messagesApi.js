9ffda167bd47cfa793327f4fdc651e9ddd97269c
shaheen2013 <coredeveloper.2013@gmail.com>
2023-05-21 20:00:51 +0600

    Added api for Add , Edit Message

--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -5,9 +5,16 @@ export const messagesApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getMessages: builder.query({
             query: (id) => `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`
-        })
+        }),
+        addMessage: builder.mutation({
+            query: (data) => ({
+                url: '/messages',
+                method: 'POST',
+                body: data
+            })
+        }),
     })
 
 })
 
-export const {useGetMessagesQuery} = messagesApi;
+export const {useGetMessagesQuery, useAddMessageMutation} = messagesApi;
